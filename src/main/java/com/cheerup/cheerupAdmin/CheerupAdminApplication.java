b3d274d5206b9f360ad47e94ddbb5f63428875c8
jelly02 <haribo__@naver.com>
2023-06-01 23:21:12 +0900

    초기 세팅

---
+++ src/main/java/com/cheerup/cheerupAdmin/CheerupAdminApplication.java
@@ -0,0 +1,16 @@
+package com.cheerup.cheerupAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerupAdminApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CheerupAdminApplication.class, args);
+
+		//test
+	}
+
+}
---
+++ src/test/java/com/cheerup/cheerupAdmin/CheerupAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheerup.cheerupAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheerupAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
